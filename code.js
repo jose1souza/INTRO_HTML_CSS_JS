1945e2a7a47fbaeb8b26c8304441e8855dde6df5
JOSE CARLOS SOUZA <14679274654@CAMPUS.MACHADO.MCH>
2025-08-19 10:54:58 -0300

    feat: Implementation searchAPI

--- code.js
+++ code.js
@@ -6,7 +6,7 @@ function changeColor() {
 titulo.addEventListener('click', changeColor);
 
 function addParagraph(){
-    const paragraphToBeAdd = document.createElement("p");
+    const paragraphToBeAdd = document.createElement('p');
     const textToBeAddText = document.
         createTextNode("Text for a added Paragraph");
     
@@ -24,9 +24,27 @@ function searchAPI(){
     // Requisition HTTP GET (requesy --> SERVER --> response)
     fetch(usersURL) // Return one Promisse
         .then(response => { // is lâmbida
-            if(response.json){ // response status == ok
-                alert('Consult completed!');
+            if(!response.ok){ // response status == ok
+               throw new Error("Error for consult API");
             }
+
+            return response.json();
+        }).then(data => {
+            const p = document.createElement('p');
+            const index = Math.floor(Math.random() * 9);
+            const textNode = document.createTextNode("Name: " +
+                data[index].name +
+                " Email: " +
+                data[index].email);
+
+            p.appendChild(textNode);
+            document.body.appendChild(p);
+        }).catch(error => {
+            const p = document.createElement('p');
+            const textNode = document.createTextNode(error.message)
+
+            p.appendChild(textNode);
+            document.body.appendChild(p);
         }); // one function
 }
 
